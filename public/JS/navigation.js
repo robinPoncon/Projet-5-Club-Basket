96718fb5784e91aa787905065267f61654ba5e42
Robin Ponçon <poncon.robin@gmail.com>
2020-11-09 16:43:22 +0100

    Finition carousel slick + responsive

--- public/JS/navigation.js
+++ public/JS/navigation.js
@@ -598,13 +598,34 @@ function rotate(e){
 }
 
 
-//$('.diapo-sponsor').slick();
+
 
 $('.diapo-sponsor').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive:
+        [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000
+                }
+            }
+        ]
 });
+
+
+
+
+$(".slick-prev.slick-arrow").text("<<");
+$(".slick-next.slick-arrow").text(">>");
 
 
 
